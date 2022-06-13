530873a277723cc46e5cb9b37f515c261b13f33f
limunosekai <powerlsh0103@gmail.com>
2022-06-13 21:58:02 +0900

    Add Navbar & Toast

--- fireship-blog/components/Navbar.tsx
+++ fireship-blog/components/Navbar.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Link from "next/link";
 
-function Navbar() {
-  return <div>Navbar</div>;
+function Navbar(props) {
+  const { username, user } = props;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} alt={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Login</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
--- fireship-blog/pages/index.tsx
+++ fireship-blog/pages/index.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { Loader } from "../components";
+import toast from "react-hot-toast";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <Loader show />
+      <button onClick={() => toast.success("hello world!")}>button</button>
     </div>
   );
 }
