bc75cb396c756aa8e9e382d2a059bf26c0f5d4a9
limunosekai <powerlsh0103@gmail.com>
2022-06-19 17:23:04 +0900

    Add ISR into PostPage

--- fireship-blog/pages/[username]/[slug].tsx
+++ fireship-blog/pages/[username]/[slug].tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import styles from "../../styles/Post.module.css";
+import { PostContent } from "../../components";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post = null;
+  let path = null;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postRef.get());
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup("posts").get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
 
 function PostPage() {
-  return <div>PostPage</div>;
+  return <main className={styles.container}></main>;
 }
 
 export default PostPage;
