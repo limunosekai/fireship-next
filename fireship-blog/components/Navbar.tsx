42e25380049cb347e21483f4f89798d7bdabd7ad
limunosekai <powerlsh0103@gmail.com>
2022-06-19 18:04:39 +0900

    Add Navbar sign-out func

--- fireship-blog/components/Navbar.tsx
+++ fireship-blog/components/Navbar.tsx
@@ -1,9 +1,18 @@
 import React, { useContext } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { UserContext } from "../lib/context";
+import { auth } from "../lib/firebase";
 
 function Navbar() {
   const { username, user } = useContext(UserContext);
+
+  const router = useRouter();
+
+  const signOut = () => {
+    auth.signOut();
+    router.reload();
+  };
 
   return (
     <nav className="navbar">
@@ -16,6 +25,9 @@ function Navbar() {
         {username && (
           <>
             <li className="push-left">
+              <button onClick={signOut}>Sign Out</button>
+            </li>
+            <li>
               <Link href="/admin">
                 <button className="btn-blue">Write Posts</button>
               </Link>
