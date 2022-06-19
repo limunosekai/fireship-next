7480059c28be66f60d29735965431e392c1d4cdc
limunosekai <powerlsh0103@gmail.com>
2022-06-19 22:24:53 +0900

    Add create post func

--- fireship-blog/pages/admin/index.tsx
+++ fireship-blog/pages/admin/index.tsx
@@ -1,10 +1,89 @@
-import { AuthCheck } from "../../components";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { AuthCheck, PostFeed } from "../../components";
+import { UserContext } from "../../lib/context";
+import { auth, firestore, serverTimestamp } from "../../lib/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import kebabCase from "lodash.kebabcase";
+import toast from "react-hot-toast";
+
+import styles from "../../styles/Admin.module.css";
+
+function PostList() {
+  const ref = firestore
+    .collection("users")
+    .doc(auth.currentUser.uid)
+    .collection("posts");
+  const query = ref.orderBy("createdAt");
+  const [querySnapshot] = useCollection(query);
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+
+  const slug = encodeURI(kebabCase(title));
+  const isValid = title.length > 3 && title.length < 100;
+
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const ref = firestore
+      .collection("users")
+      .doc(uid)
+      .collection("posts")
+      .doc(slug);
+
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: "# hello world!",
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    await ref.set(data);
+    toast.success("Post Created!");
+    router.push(`/admin/${slug}`);
+  };
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Article!"
+        className={styles.input}
+      />
+      <p>
+        <strong>Slug:</strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Create New Post
+      </button>
+    </form>
+  );
+}
 
 function AdminPostsPage() {
   return (
     <main>
       <AuthCheck>
-        <div>123</div>
+        <PostList />
+        <CreateNewPost />
       </AuthCheck>
     </main>
   );
